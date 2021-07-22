eb076b0f3b9ab0a6a5079daf1994c6da29b1caf0
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-07-21 22:45:47 -0700

    Initial commit. Code written by everyone in-class.

---
+++ ramayan2/src/it/java/edu/pdx/cs410J/ramayan2/KataIT.java
@@ -0,0 +1,73 @@
+package edu.pdx.cs410J.ramayan2;
+
+
+import edu.pdx.cs410J.InvokeMainTestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
+
+class KataIT extends InvokeMainTestCase {
+
+  @Test
+  void invokingMainWithNoArgumentsHasExitCodeOf1() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class);
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+
+  @Test
+  void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class);
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  }
+
+  @Test
+  void simplestCaseMultiplication() {
+
+    InvokeMainTestCase.MainMethodResult result1 = invokeMain(Kata.class, "20", "5","*");
+    assertThat(result1.getTextWrittenToStandardOut(), equalTo(100+"\r\n"));
+
+  }
+  @Test
+  void simpleCaseDivision(){
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "20", "5", "/");
+    assertThat(result.getTextWrittenToStandardOut(), equalTo(4+"\r\n"));
+  }
+  @Test
+  void simpleCaseAdditionAndSubtraction(){
+
+    InvokeMainTestCase.MainMethodResult result2 = invokeMain(Kata.class, "4", "2", "+","3","-");
+    assertThat(result2.getTextWrittenToStandardOut(), equalTo(3+"\r\n"));
+  }
+
+  @Test
+  void simpleCaseDMultiplicationAndSubstraction(){
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "3", "5", "8","*","7","-","*");
+    assertThat(result.getTextWrittenToStandardOut(), equalTo(1089+"\r\n"));
+  }
+
+  @Test
+  void simpleCaseDMultiplicationDivisionAndSubstraction(){
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "3", "5", "8","*","7","-","*","/","10");
+    assertThat(result.getTextWrittenToStandardOut(), equalTo(10+"\r\n"));
+  }
+
+
+  @Test
+  void simpleCaseDMultiplicationAdditionSubstractionAndDivision() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "3", "5", "8", "*", "7", "-", "*", "+", "10", "/", "5");
+    assertThat(result.getTextWrittenToStandardOut(), equalTo(5 + "\r\n"));
+
+  }
+
+//  @Test
+//  void simpleCaseSquareRoot(){
+//    //is failing because we pop 2 numbers that gives an exception
+//    //but why did we moved out. popping in the case is good idea right
+//    //Popping is good but we need to pop according to length. lets do it in next week. ok
+//    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "9","SQRT");
+//    assertThat(result.getTextWrittenToStandardOut(), equalTo(3+"\r\n"));
+//  }
+
+}
---
+++ ramayan2/src/main/java/edu/pdx/cs410J/ramayan2/Kata.java
@@ -0,0 +1,60 @@
+package edu.pdx.cs410J.ramayan2;
+
+import java.util.Stack;
+
+/**
+ * A class for getting started with a code kata
+ *
+ * Use IntelliJ's "Refactor | Rename..." command to change the name of this
+ * class (and its tests).
+ */
+public class Kata {
+
+
+  public static void main(String[] args) {
+
+    if (args.length == 0) {
+      System.err.println("Missing command line arguments");
+      System.exit(1);
+    }
+
+    Stack<Integer> myStack = new Stack<>();
+    //String regex = "(\\d+(?:\\.\\d+)?)";
+    int res = 0;
+    for (String arg : args) {
+      if (arg.matches("\\d+")) {
+          myStack.push(Integer.parseInt(arg));
+      } else {
+        int num1 = myStack.pop();
+        int num2 = myStack.pop();
+        switch (arg) {
+          case "+":
+            res = num1 + num2;
+            myStack.push(res);
+            break;
+          case "-":
+            res = num2 - num1;
+            myStack.push(res);
+            break;
+
+          case "*":
+            res = num1 * num2;
+            myStack.push(res);
+            break;
+
+          case "/":
+            res = num2 / num1;
+            myStack.push(res);
+            break;
+
+          case "SQRT":
+            res = (int) Math.sqrt(myStack.pop());
+            break;
+        }
+      myStack.push(res);
+      }
+    }
+    System.out.println(myStack.pop());
+
+  }
+}
